ac005d940ae61aad9e447a8c6ab58a44f33e36fb
Pratham Bist <56242623+pr-atha-m@users.noreply.github.com>
2023-09-24 16:03:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,23 +1,38 @@
-package com.pratham.wellsfargo;
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
-public class Main {
+/* AlertPopup.css */
+.popup-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+}
 
-	public static void main(String[] args) {
-		
-		Connection connection = null;
-		String url = "jdbc:mariadb://localhost:3306/mysql";
-		String user = "root";
-		String pwd = "password";
-		
-		try {
-			connection = DriverManager.getConnection(url,user,pwd);
-			}catch(SQLException e) {
-				e.printStackTrace();
-			}
-		
-		System.out.println("Success");
-		
-	}
+.popup {
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  text-align: center;
+  max-width: 300px;
+  position: relative; /* Add this line to make the close button relative to the popup */
+}
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+
+.success {
+  color: #4caf50; /* Green color for success */
+}
+
+.error {
+  color: #f44336; /* Red color for error */
 }
